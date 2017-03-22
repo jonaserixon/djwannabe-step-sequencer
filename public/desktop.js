91f659620e43ce68ccf48ee9527f1b7b1a20da37
Jonas Erixon <je222zj@student.lnu.se>
2017-03-22 15:09:18 +0100

    testing

--- public/desktop.js
+++ public/desktop.js
@@ -1,5 +1,40 @@
+"use strict";
+
+let wrapper = document.getElementById('wrapper');
 
 $( function() {
-  $( "#draggableBox" ).draggable({ snap: ".main-timeline" });
-
+  $('#draggableBox').draggable({
+    snap: ".main-timeline",
+    drag: function( event, ui ) {
+      sound.stop();
+    }
+  });
 });
+
+/**
+ * Logic running when a div is dropped
+ */
+$('.main-timeline').droppable({
+  drop: function( event, ui ) {
+    $( this );
+    wrapper.appendChild(playButton);
+  }
+});
+
+var sound = new Howl({
+  src: ['loop1.wav'],
+  volume: 0.5,
+  onend: function() {
+    console.log('Finished!');
+  }
+});
+
+let playButton = document.createElement('div');
+playButton.setAttribute('id','play-button');
+playButton.textContent = 'PLAY!';
+
+playButton.addEventListener('click', function() {
+  console.log('spela samplet!');
+  sound.play();
+});
+
